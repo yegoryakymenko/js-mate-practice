7c0d9b24548342e76b5651b95d9a85e5e3d8de65
Karpenko Lily <kl2.karpenko@gmail.com>
2018-10-18 07:20:20 +0300

    Update script.js

--- test-practice-part/script.js
+++ test-practice-part/script.js
@@ -1,20 +1,24 @@
 // 1
 let a = function length(arr) {
 	console.log(arr.length);
+	// everything is right but get used to return a meaning not console.log it
 }
 
 let b = [1,2,3,4,5];
 a(b);
 // 2
 function countArg() {
-		console.log(arguments.length);
+	console.log(arguments.length);
+	// everything is right but get used to return a meaning not console.log it
 }
 countArg(1,2,3,4,5);
 // 3
 let c = function emptyElements(arr) {
 	for (i = 0; i < arr.length; i ++) {
+		// here better to check that it's empty or undefined
 		if(arr[i] == '') {
 			delete arr[i];
+			// method is Array works only like this: Array.isArray(arr[i])
 		} else if(arr[i].isArray) {
 			return emptyElements(arr[i]);
 		}
@@ -24,6 +28,7 @@ let c = function emptyElements(arr) {
 // 4
 let d = function reverseSort(arr) {
 	console.log(arr.reverse())
+	// return
 }
 // 5
 var date = new Date();
@@ -45,6 +50,8 @@ console.log(e(date));
 setTimeout(function() {
 	console.log(Object.keys(date).length);
 }, 1000)
+//this should be wrapped in a function so I can call it whereever I want 
+
 // 7
 var check = function() {
 	var value = prompt("Enter your name");
@@ -57,3 +64,4 @@ var check = function() {
 	}
 }
 check();
+// your function works every 2 seconds and never stops, you should consider to stop interval if name is the right result

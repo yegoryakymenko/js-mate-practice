30904ea96582391a6b0bee623330fc41fef2155e
Yegor Yakymenko <intheskyp@gmail.com>
2018-11-27 14:25:04 +0200

    Fixing

--- jquery-practice/sctipt.js
+++ jquery-practice/sctipt.js
@@ -9,8 +9,10 @@ $(function() {
     let nameValue   = $name.val();
     let surnamValue = $surname.val();
     if(nameValue && surnamValue) {
-      let li = $(`<li class="list-group-item"> <span id="name">${nameValue}</span> <span id="surname">${surnamValue}</span> <button class="btn btn-danger d-none">Remove</button>  </li>`);
+      let li = $(`<li class="list-group-item"> <span class="name">${nameValue}</span> <span class="surname">${surnamValue}</span> <button class="btn btn-danger d-none">Remove</button>  </li>`);
       $ul.append(li);
+      $name.val('');
+      $surname.val('');
     }
   })
   // hover function
@@ -28,39 +30,32 @@ $(function() {
     $(this).closest('li').remove();
   });
   // value change function
-  $ul.on('click', 'span#name', function(event) {
+  $ul.on('click', 'span.name', function(event) {
   //  console.log($(this).text());
       let changeName = $('<input type="text" class="changeName">');
-      let spanName = $('span#name');
-      spanName.after(changeName);
+      let spanName = $('span.name');
+      $(this).after(changeName); 
+      changeName.val($(this).text());
       changeName.on('blur', function() {
         let inputValue = changeName.val();
    //   console.log(inputValue);
         spanName.html('');
         spanName.html(inputValue);
-        changeName.addClass('d-none');
+        changeName.remove();
       })
   });
-  $ul.on('click', 'span#surname', function(event) {
+  $ul.on('click', 'span.surname', function(event) {
     //  console.log($(this).text());
         let changeSurname = $('<input type="text" class="changeSurname">');
-        let spanSurname = $('span#surname');
-        spanSurname.after(changeSurname);
+        let spanSurname = $('span.surname');
+        $(this).after(changeSurname);
+        changeSurname.val($(this).text());  
         changeSurname.on('blur', function() {
           let inputValue = changeSurname.val();
      //   console.log(inputValue);
           spanSurname.html('');
           spanSurname.html(inputValue);
-          changeSurname.addClass('d-none');
+          changeSurname.remove();
         })
     });
 });
-
-
-
-// при клике на спан, должен появиться инпут рядом со спаном.
-// должна быть переменная, в которую идет инфа из изначального спана
-// после, у нас появялется инпут с значением спана
-// при потере фокуса, значение инпута запишется в переменную
-// а инпут на заменится на спан со значением из переменной
-// в него должна записаться инфа, и при потере фокуса инфа из инпута полетит в спан
